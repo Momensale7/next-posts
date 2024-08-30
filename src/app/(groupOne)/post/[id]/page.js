141223759b176c131f768dc42ef0bbf4835de133
momen <momensalah2468@gmail.com>
2024-08-30 23:27:25 +0300

    not Found

--- src/app/(groupOne)/post/[id]/page.js
+++ src/app/(groupOne)/post/[id]/page.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import Cart from '../../blog/components/Cart'
 import NextPrev from '../../blog/components/NextPrev';
+import { notFound } from 'next/navigation';
 
 
 const page = async ({ params }) => {
-  const url = `https://next-posts-ten.vercel.app/api/posts?id=${params.id}`;
-  const res = await fetch(url);
-  const post =await res.json()
-  const id = Number(params.id)
-  return (
-    <div>
-      <div className='grid grid-cols-12 mt-10'>
-        <Cart body={post.body} title={post.title} id={post.id} image={post.photo} />
-      </div>
-      <div className='grid grid-cols-12 mt-3 pb-32'>
-      <NextPrev id={id} />
-      </div>
-      </div>
-      
-   
-  )
+  try {
+    const id = Number(params.id)
+    const url = `https://next-posts-ten.vercel.app/api/posts?id=${params.id}`;
+    const res = await fetch(url);
+    const post =await res.json()
+    return (
+      <div>
+        <div className='grid grid-cols-12 mt-10'>
+          <Cart body={post.body} title={post.title} id={post.id} image={post.photo} />
+        </div>
+        <div className='grid grid-cols-12 mt-3 pb-32'>
+        <NextPrev id={id} />
+        </div>
+        </div>
+        
+     
+    )
+  } catch (error) {
+    notFound()
+  }
+
 }
 
 export default page
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='mt-12 text-red-500 text-center text-2xl '>
+      page not found
+    </div>
+  )
+}
+
+export default NotFound
